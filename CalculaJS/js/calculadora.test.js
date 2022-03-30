8dbc3b34bc65e99e97146ce4e4f6ab2f71a05344
vagnercardozo <vagnercardozo2002@gmail.com>
2022-03-30 11:19:18 -0300

    Mudando a importacao das funçoes no arquivo de teste

--- CalculaJS/js/calculadora.test.js
+++ CalculaJS/js/calculadora.test.js
@@ -1,17 +1,20 @@
-const calc = require('./calc');
+import {soma, subtracao, multiplicacao, divisao} from './calc'
 
 it('Soma de dois positivos',()=>{
-    expect(calc.soma(2,4)).toBe(6)
+    expect(soma(2,4)).toBe(6)
 })
 it('Soma de dois negativos',()=>{
-    expect(calc.soma (-2,4)).toBe(2)
+    expect(soma (-2,4)).toBe(2)
 })
 it('Subtracao de dois positivos',()=>{
-    expect(calc.subtracao(2,4)).toBe(-2)
+    expect(subtracao(2,4)).toBe(-2)
 })
 it('Subtracao de dois negativos',()=>{
-    expect(calc.subtracao(-2,-4)).toBe(2)
+    expect(subtracao(-2,-4)).toBe(2)
 })
 it('Multiplicação de dois positivos',()=>{
-    expect(calc.multiplicacao(2,4)).toBe(8)
+    expect(multiplicacao(2,4)).toBe(8)
+})
+it('Divisão de dois positivos',()=>{
+    expect(divisao(4,2)).toBe(2)
 })
