5ff1decf7d6425e1c2b89df7730297c314876292
vagnercardozo <vagnercardozo2002@gmail.com>
2022-03-30 11:19:39 -0300

    Mudando a importacao das funçoes no arquivo de teste

---
+++ CalculaJS/js/calc.js
@@ -0,0 +1,14 @@
+export {soma,subtracao,multiplicacao,divisao} 
+
+function soma(a,b){
+    return a + b;
+}
+function subtracao(a,b){   
+    return a - b; 
+}
+function multiplicacao(a,b){   
+    return a * b; 
+}
+function divisao(a,b){
+    return a / b; 
+}
