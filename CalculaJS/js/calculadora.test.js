e42a582475f8bde080e41424fff4c9aab12ae249
vagnercardozo <vagnercardozo2002@gmail.com>
2022-03-30 10:44:04 -0300

    Mudando nome do arquivo das funcoes

--- CalculaJS/js/calculadora.test.js
+++ CalculaJS/js/calculadora.test.js
@@ -1,17 +1,17 @@
-const soma = require('./soma');
+const calc = require('./calc');
 
 it('Soma de dois positivos',()=>{
-    expect(soma.soma(2,4)).toBe(6)
+    expect(calc.soma(2,4)).toBe(6)
 })
 it('Soma de dois negativos',()=>{
-    expect(soma.soma (-2,4)).toBe(2)
+    expect(calc.soma (-2,4)).toBe(2)
 })
 it('Subtracao de dois positivos',()=>{
-    expect(soma.subtracao(2,4)).toBe(-2)
+    expect(calc.subtracao(2,4)).toBe(-2)
 })
 it('Subtracao de dois negativos',()=>{
-    expect(soma.subtracao(-2,-4)).toBe(2)
+    expect(calc.subtracao(-2,-4)).toBe(2)
 })
 it('Multiplicação de dois positivos',()=>{
-    expect(soma.multiplicacao(2,4)).toBe(8)
+    expect(calc.multiplicacao(2,4)).toBe(8)
 })
--- CalculaJS/js/soma.js
+++
@@ -1,9 +0,0 @@
-exports.soma = (a,b) => {
-    return a + b;
-}
-exports.subtracao = (a,b) =>{   
-    return a - b; 
-}
-exports.multiplicacao = (a,b) =>{   
-    return a * b; 
-}
