15bcb955a35f978432ddf77a7bb258640ba33b6b
vagnercardozo <vagnercardozo2002@gmail.com>
2022-03-31 11:46:14 -0300

    Estilo Calc

--- CalculaJS/index.js
+++ CalculaJS/index.js
@@ -1,9 +1,14 @@
-import {calculaDelta,calculaBhaskara} from '../js'
+import { calculaBhaskara, calculaDelta } from './js/calc.js'
 
 function calcular(){
-    delta = calculaDelta(2,8,-24)
-    bhaskara = calculaBhaskara(2,8,256)
-
-    console.log("testeeeeee")
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let c = document.getElementById('c').value;
+    let delta = calculaDelta(a,b,c);
+    console.log(delta);
+    let bhaskara = calculaBhaskara(a,b,delta);
+    console.log(bhaskara);
+    document.getElementById('x1').innerHTML = '<b>'+bhaskara.x1+'</b>'
+    document.getElementById('x2').innerHTML = '<b>'+bhaskara.x2+'</b>'
 }
-document.getElementById('calcular').addEventListener('click', calcular)
+document.getElementById('calcular').addEventListener('click', calcular);
--- CalculaJS/js/calc.js
+++ CalculaJS/js/calc.js
@@ -4,10 +4,9 @@ function calculaDelta(a,b,c) {
     return (Math.pow(b,2))-4*a*c;
 }
 function calculaBhaskara(a,b,delta){
-    let x1,x2;
 
-    x1 = (((-b) - (Math.sqrt(delta))) / (2*a)) 
-    x2 = (((-b) + (Math.sqrt(delta))) / (2*a)) 
+    let x1 = (((-b) + (Math.sqrt(delta))) / (2*a)) 
+    let x2 = (((-b) - (Math.sqrt(delta))) / (2*a)) 
 
     return {x1,x2}
 }
