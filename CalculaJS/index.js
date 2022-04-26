47330487f3822ba76f8b5528fa7a0ab79dcd722b
vagnercardozo <vagnercardozo2002@gmail.com>
2022-04-26 09:34:26 -0300

    finalizada calculadora js

--- CalculaJS/index.js
+++ CalculaJS/index.js
@@ -1,25 +1,27 @@
-import { calculaBhaskara, calculaDelta } from './js/calc.js'
+import { calculaBhaskara, calculaDelta } from "./js/calc.js";
 
-document.getElementById('calcular').addEventListener('click', calcular);
-document.getElementById('exibirNovamente').addEventListener('click', exibirNovamente);
+document.getElementById("calcular").addEventListener("click", calcular);
+document
+  .getElementById("exibirNovamente")
+  .addEventListener("click", exibirNovamente);
 
-
-window.onload = function() {
-    document.getElementById('resultado').style.display = "none"
+window.onload = function () {
+  document.getElementById("resultado").style.display = "none";
 };
-function calcular(){
-    let a = document.getElementById('a').value;
-    let b = document.getElementById('b').value;
-    let c = document.getElementById('c').value;
-    let delta = calculaDelta(a,b,c);
-    let bhaskara = calculaBhaskara(a,b,delta);
-    document.getElementById('principal').style.display = "none"
-    exibirCalculo()
-}
-function exibirCalculo(){
-    document.getElementById('resultado').style.display = "block"
-}
-function exibirNovamente(){
-    document.getElementById('resultado').style.display = "none"
-    document.getElementById('principal').style.display = "block"
+function calcular() {
+  let a = document.getElementById("a").value;
+  let b = document.getElementById("b").value;
+  let c = document.getElementById("c").value;
+  let delta = calculaDelta(a, b, c);
+  if (delta >= 0) {
+    let bhaskara = calculaBhaskara(a, b, delta);
+    document.getElementById("x1").innerHTML = "x1 = " + bhaskara.x1;
+    if (bhaskara.x2) {
+      document.getElementById("x2").innerHTML = "x2 = " + bhaskara.x2;
+    }       
+  } else {
+    document.getElementById("x1").innerHTML = "Equação não possui raizes";
+    document.getElementById("x2").innerHTML = "";
+  }
+  document.getElementById("resultado").style.display = "block";
 }
--- CalculaJS/js/calc.js
+++ CalculaJS/js/calc.js
@@ -1,12 +1,16 @@
-export {calculaDelta,calculaBhaskara} 
+export { calculaDelta, calculaBhaskara };
 
-function calculaDelta(a,b,c) {
-    return (Math.pow(b,2))-4*a*c;
+function calculaDelta(a, b, c) {
+  return Math.pow(b, 2) - 4 * a * c;
 }
-function calculaBhaskara(a,b,delta){
+function calculaBhaskara(a, b, delta) {
+  if (delta == 0) {
+    let x1 = -b / (2 * a);
+    return { x1 };
+  } else {
+    let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    let x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
-    let x1 = (((-b) + (Math.sqrt(delta))) / (2*a)) 
-    let x2 = (((-b) - (Math.sqrt(delta))) / (2*a)) 
-
-    return {x1,x2}
+    return { x1, x2 };
+  }
 }
